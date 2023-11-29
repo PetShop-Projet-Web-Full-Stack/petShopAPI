755ca613ef229d1488b9cab234214182170ec55b
Peyronon-Arno <arno.peyronon@gmail.com>
2023-11-29 22:46:12 +0100

    medias filled

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,14 @@ use Database\Seeders\AnimalSeeder;
 use Database\Seeders\PetShopSeeder;
 use Database\Seeders\SpeciesSeeder;
 use Database\Seeders\QuestionSeeder;
+use Database\Seeders\MediasSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this->call([
+            MediasSeeder::class,
             SpeciesSeeder::class,
             RaceSeeder::class,
             PetShopSeeder::class,
